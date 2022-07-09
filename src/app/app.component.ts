31a62dd64aa0b3f01d05a3c49b45be12825795b7
Sampath Sanjeeva Billava <Sampath.Billava@novigosolutions.com>
2022-07-09 10:55:21 +0530

    check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'templateDrivenForms';
+  topics = ["Angular", "React", "Vue"];
+  topicHasError = true;
+
+  userModel = new User("Rob", "rob@test.com", 1234567890, "default", "morning", true);
+
+
+  validateTopic(value: any) {
+    if (value === "default") {
+      this.topicHasError = true;
+    }
+    else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit() {
+    console.log(this.userModel)
+  }
 }
